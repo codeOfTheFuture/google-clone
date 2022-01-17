f150e44c478f505334e64db461ccbe42e8d27d5a
Jeff Oliver <jeffoliver3384@gmail.com>
2022-01-17 04:25:12 -0500

    Header component ui complete

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Avatar from "./Avatar";
+import { ViewGridIcon } from '@heroicons/react/solid';
+
+const Header: React.FC = () => {
+  return (
+    <header className='flex w-full p-5 justify-between text-sm text-gray-700'>
+      <div className='flex space-x-4 items-center'>
+        <a className='link'>About</a>
+        <a className='link'>Store</a>
+      </div>
+      <div className='flex space-x-4 items-center'>
+        <a className='link'>Gmail</a>
+        <a className='link'>Images</a>
+        {/*  Icon */}
+        <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+
+        <Avatar url={'http://coaching.papareact.com/ai9'} />
+      </div>
+    </header>
+  )
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkmode: false,
+  darkMode: "media",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
